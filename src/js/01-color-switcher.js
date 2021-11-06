4027366bcb85352299c6195fe9b2aeef789bb5f0
SerheijTsotsorin <84991425+SerheijTsotsorin@users.noreply.github.com>
2021-11-06 22:20:53 +0200

    task-1 add skript change bodycolor

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body')
+let intervalId = null;
+
+stopBtn.getAttribute('disabled', true);
+
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+function onStart() {
+    intervalId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+};
+
+function onStop() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
